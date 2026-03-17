2620279cc5e8927c1eb3d0c6013227dc84f75684
easonysliu <easonysliu@tencent.com>
2026-03-17 11:47:25 +0800

    Add author name on cover: 长夜观心

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-62ca31e2.js',
+                path_to_root + 'searchindex-e84e7d31.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
